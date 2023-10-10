3951cc4688b5f1c95e46dbd1f3375f5a94067725
Swift27 <porschgregor@gmail.com>
2023-10-10 12:22:31 +0200

    Set up

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ["12px", "16px"],
+      sm: ["14px", "20px"],
+      base: ["16px", "19.5px"],
+      lg: ["18px", "21.94px"],
+      xl: ["20px", "24.38px"],
+      "2xl": ["24px", "29.26px"],
+      "3xl": ["28px", "50px"],
+      "4xl": ["48px", "58px"],
+      "8xl": ["96px", "106px"],
+    },
+    extend: {
+      fontFamily: {
+        publicSans: ["Public Sans", "sans-serif"],
+      },
+      colors: {
+        "dark-blue": "hsl(233, 26%, 24%)",
+        "lime-green": "hsl(136, 65%, 51%)",
+        "bright-cyan": "hsl(192, 70%, 51%)",
+        "grayish-blue": "hsl(233, 8%, 62%)",
+        "light-grayish-blue": "hsl(220, 16%, 96%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 };
